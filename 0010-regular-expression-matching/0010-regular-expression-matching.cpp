f9e22966ed905ac25ac9a13a8793597d80f80b21
Tripti Nishad <triptinishad011@gmail.com>
2026-08-30 08:30:51 +0530

    Time: 2 ms (58.21%), Space: 9.1 MB (70.9%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int m = s.length();
+        int n = p.length();
+
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
+
+        // Empty string matches empty pattern
+        dp[0][0] = true;
+
+        // Empty string with patterns like a*, a*b*, a*b*c*
+        for (int j = 2; j <= n; j++) {
+            if (p[j - 1] == '*') {
+                dp[0][j] = dp[0][j - 2];
+            }
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+
+                // Normal character or '.'
+                if (p[j - 1] == '.' || p[j - 1] == s[i - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+
+                // '*'
+                else if (p[j - 1] == '*') {
+
+                    // Use '*' as zero occurrences
+                    dp[i][j] = dp[i][j - 2];
+
+                    // Use '*' for one or more occurrences
+                    if (p[j - 2] == '.' || p[j - 2] == s[i - 1]) {
+                        dp[i][j] = dp[i][j] || dp[i - 1][j];
+                    }
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+};
